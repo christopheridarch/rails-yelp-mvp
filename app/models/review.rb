752db55b8008f4023268a04751c9ea5242b6ca40
christophe ridarch <christophe.ridarch@gmail.com>
2017-11-22 21:22:48 +0100

    Add url picture

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,6 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
   validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..5), message: "%{value} is not between 0 and 5" }
-
-
 end
 
 # A review must have a parent restaurant.
